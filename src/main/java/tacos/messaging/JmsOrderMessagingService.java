f606d840b0efe35cb2414bfa0aaaa4c9b8ca1a9d
hjzboss <425331389@qq.com>
2020-11-17 17:46:26 +0800

    jms

---
+++ src/main/java/tacos/messaging/JmsOrderMessagingService.java
@@ -0,0 +1,35 @@
+package tacos.messaging;
+
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Service;
+import tacos.Order;
+
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+
+@Service
+public class JmsOrderMessagingService implements OrderMessagingService {
+    private final JmsTemplate jms;
+    private final Destination orderQueue;
+
+    public JmsOrderMessagingService(JmsTemplate jms, Destination orderQueue) {
+        this.jms = jms;
+        this.orderQueue = orderQueue;
+    }
+
+    //发送消息
+    @Override
+    public void sendOrder(Order order) {
+        //第一个参数为目的地
+        //第二个参数为对象，jms会使用MappingJackson2MessageConverter来将对象转换为消息
+        //第三个参数表示对消息的后期处理
+        jms.convertAndSend(orderQueue,order,this::addOrderSource);
+    }
+
+    //将消息加上额外的信息，在这里给订单消息加上了来源，来自web
+    private Message addOrderSource(Message message) throws JMSException {
+        message.setStringProperty("X_ORDER_SOURCE", "WEB");
+        return message;
+    }
+}
---
+++ src/main/java/tacos/messaging/MessagingConfig.java
@@ -0,0 +1,36 @@
+package tacos.messaging;
+
+import org.apache.activemq.artemis.jms.client.ActiveMQQueue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import tacos.Order;
+
+import javax.jms.Destination;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class MessagingConfig {
+
+    //配置消息转换器
+    @Bean
+    public MappingJackson2MessageConverter messageConverter() {
+        MappingJackson2MessageConverter messageConverter =
+                new MappingJackson2MessageConverter();
+        messageConverter.setTypeIdPropertyName("_typeId");
+
+        Map<String, Class<?>> typeIdMappings = new HashMap<String, Class<?>>();
+        typeIdMappings.put("order", Order.class);
+        messageConverter.setTypeIdMappings(typeIdMappings);
+
+        return messageConverter;
+    }
+
+    //指定消息目的地
+    @Bean
+    public Destination orderQueue(){
+        return new ActiveMQQueue("tacocloud.order.queue");
+    }
+
+}
