d18b2624d5c60cb824ddedbe58d780ce19f76cd5
hjzboss <425331389@qq.com>
2020-11-26 17:14:55 +0800

    重构

---
+++ tacocloud-security/src/main/java/tacos/security/security/RegistrationForm.java
@@ -0,0 +1,26 @@
+package tacos.security.security;
+
+import lombok.Data;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import tacos.User;
+
+@Data
+public class RegistrationForm {
+
+    private String username;
+    private String password;
+    private String confirm;
+    private String fullname;
+    private String street;
+    private String city;
+    private String state;
+    private String zip;
+    private String phone;
+
+    public User toUser(PasswordEncoder passwordEncoder) {
+        return new User(
+                username, passwordEncoder.encode(password),
+                fullname, street, city, state, zip, phone);
+    }
+
+}
---
+++ tacocloud-security/src/main/java/tacos/security/security/SecurityConfig.java
@@ -0,0 +1,70 @@
+package tacos.security.security;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
+
+    public SecurityConfig(@Qualifier("userRepositoryUserDetailsService") UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/design", "/orders")
+                .access("hasRole('ROLE_USER')")
+                .antMatchers("/**").access("permitAll")
+
+                .and()
+                .formLogin()
+                .loginPage("/login")
+
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+
+                .and()
+                .csrf()
+                .ignoringAntMatchers("/h2-console/**")
+                .and()
+                .csrf()
+                .ignoringAntMatchers("/druid/**")
+
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+        ;
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new StandardPasswordEncoder("53cr3t");
+    }
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)
+            throws Exception {
+
+        auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(encoder());
+
+    }
+}
+
