89c5426bb61840a1bc34f3a12d356152608c085b
hjzboss <425331389@qq.com>
2020-12-31 19:07:53 +0800

    ui

---
+++ tacocloud/tacocloud-ui/ui/src/components/Search.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+  <h1>查询taco</h1>
+  <input type="text" v-model="id">
+  <input type="button" @click="search" value="查询">
+  <h3>{{msg}}</h3>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Search",
+  data(){
+    return{
+      id:"",
+      url:"http://localhost:8080/design/",
+      msg:""
+    }
+  },
+  methods:{
+    search(){
+      this.$http.get(this.url+this.id).then(res=>{
+        if(res.status == 200){
+          this.msg = res.data;
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
