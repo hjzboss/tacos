f7aba7fc2c03a8bf377dc6c86e80ecfe3b203bcb
hjzboss <425331389@qq.com>
2021-01-19 22:57:10 +0800

    rabbitmq

--- tacocloud/tacocloud-kitchens/src/main/java/tacos/kitchen/messaging/rabbit/RabbitConfig.java
+++ tacocloud/tacocloud-kitchens/src/main/java/tacos/kitchen/messaging/rabbit/RabbitConfig.java
@@ -4,13 +4,13 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-/*配置rabbitmq的初始化*/
+/*配置rabbitmq的初始化
+* 也可使用manage界面进行初始化，或者使用AmpqAdmin进行初始化
+* */
 @Profile({"rabbitmq-template", "rabbitmq-listener"})
 @Configuration
 public class RabbitConfig {
